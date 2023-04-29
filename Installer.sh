c130fd6c9eaf49344caa8be894fd3726d950555e
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-04-29 16:40:56 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,6 +3,7 @@
 sudo pacman -Sy
 clear
 sudo pacman -S picom thunar polybar nitrogen spotify-launcher lxappearance
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 
@@ -12,6 +13,7 @@ else
 sudo curl -L https://raw.githubusercontent.com/will8211/unimatrix/master/unimatrix.py -o /usr/local/bin/unimatrix
 sudo chmod a+rx /usr/local/bin/unimatrix
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -63,6 +65,7 @@ sudo chmod +x cd $HOME/.config/rofi/launcher.sh
 chmod -R a+rw $HOME/.config/rofi/
 cd $HOME
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -76,15 +79,16 @@ echo ROFI CONFIG IS MADE AND APPLIED
 # INSTALL THE PICOM CONFIGURATION
 if [ -d "$HOME/.config/picom/" ]; then
 sudo mkdir $HOME/.config/picom/backup/
-sudo mv -f $HOME/.config/picom/picom.conf $HOME/.config/picom/backup/
-sudo cp -f ./picom-config/picom.conf $HOME/.config/picom/
+sudo mv -f $HOME/.config/picom/picom.conf $HOME/.config/picom/backup/picom.conf
+sudo cp -f ./picom-config/picom.conf $HOME/.config/picom/picom.conf
 chmod -R a+rw $HOME/.config/picom/
 chmod -R a+rw $HOME/.config/picom/backup/
 else
 sudo mkdir $HOME/.config/picom/
-sudo cp -f ./picom-config/picom.conf $HOME/.config/picom/
+sudo cp -f ./picom-config/picom.conf $HOME/.config/picom/picom.conf
 chmod -R a+rw $HOME/.config/picom/
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -97,12 +101,12 @@ echo PICOM CONFIG IS MADE AND APPLIED
 
 # INSTALL THE FONTS AND UPDATE THEN IN THE SYSTEM
 if [ -d "$HOME/.local/share/fonts/Google Fonts/" ]; then
-sudo cp -f ./fonts/GrapeNuts-Regular.ttf $HOME/.local/share/fonts/
-sudo cp -f ./fonts/Icomoon-Feather.ttf $HOME/.local/share/fonts/
-sudo cp -f ./fonts/losevka-Nerd-Font-Complete.ttf $HOME/.local/share/fonts/
-sudo cp -f ./fonts/JetBrains-Mono-Nerd-Font-Complete.ttf $HOME/.local/share/fonts/
+sudo cp -f ./fonts/GrapeNuts-Regular.ttf $HOME/.local/share/fonts/GrapeNuts-Regular.ttf
+sudo cp -f ./fonts/Icomoon-Feather.ttf $HOME/.local/share/fonts/Icomoon-Feather.ttf
+sudo cp -f ./fonts/losevka-Nerd-Font-Complete.ttf $HOME/.local/share/fonts/losevka-Nerd-Font-Complete.ttf
+sudo cp -f ./fonts/JetBrains-Mono-Nerd-Font-Complete.ttf $HOME/.local/share/fonts/JetBrains-Mono-Nerd-Font-Complete.ttf
 sudo mkdir $HOME/.local/share/fonts/Google Fonts/Kiwi Maru/
-sudo cp -f ./fonts/Kiwi_Maru_Regular.14.ttf $HOME/.local/share/fonts/Google Fonts/Kiwi Maru/
+sudo cp -f ./fonts/Kiwi_Maru_Regular.14.ttf $HOME/.local/share/fonts/Google Fonts/Kiwi Maru/Kiwi_Maru_Regular.14.ttf
 else
 sudo cp -f ./fonts/GrapeNuts-Regular.ttf $HOME/.local/share/fonts/
 sudo cp -f ./fonts/Icomoon-Feather.ttf $HOME/.local/share/fonts/
@@ -113,6 +117,7 @@ sudo mkdir $HOME/.local/share/fonts/Google Fonts/Kiwi Maru/
 sudo cp -f ./fonts/Kiwi_Maru_Regular.14.ttf $HOME/.local/share/fonts/Google Fonts/Kimi Maru/
 fi
 sudo fc-cache
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -129,15 +134,16 @@ echo FONT-CACHE SUCESSFULY UPDATED
 # MAKE A BACKUP OF YOUR OLD I3 CONFIG AND ACTIVE THE RICE ONE
 if [ -d "$HOME/.config/i3" ]; then
 sudo mkdir $HOME/.config/i3/backup
-sudo mv -f $HOME/.config/i3/config $HOME/.config/i3/backup/
-sudo cp -f ./i3-config/config $HOME/.config/i3/
+sudo mv -f $HOME/.config/i3/config $HOME/.config/i3/backup/config
+sudo cp -f ./i3-config/config $HOME/.config/i3/config
 chmod -R a+rw $HOME/.config/i3/
 chmod -R a+rw $HOME/.config/i3/backup/
 else
 sudo mkdir $HOME/.config/i3
-sudo cp -f ./i3-config/config $HOME/.config/i3
+sudo cp -f ./i3-config/config $HOME/.config/i3/config
 chmod -R a+rw $HOME/.config/i3/
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -154,14 +160,15 @@ echo I3 CONFIG IS MADE AND APPLIED
 # MAKE A BACKUP OF YOUR OLD POLYBAR CONFIG AND ACTIVE THE RICE ONE
 if [ -d "/etc/polybar/" ]; then
 sudo mkdir /etc/polybar/backup
-sudo mv -f /etc/polybar/config.ini /etc/polybar/backup/
-sudo cp -f ./polybar-config/config.ini /etc/polybar/
+sudo mv -f /etc/polybar/config.ini /etc/polybar/backup/config.ini
+sudo cp -f ./polybar-config/config.ini /etc/polybar/config.ini
 chmod -R a+rw $HOME/.config/polybar/
 chmod -R a+rw $HOME/.config/polybar/backup/
 else
-sudo cp -f ./polybar-config/config.ini /etc/polybar/
+sudo cp -f ./polybar-config/config.ini /etc/polybar/config.ini
 chmod -R a+rw $HOME/.config/polybar/
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -179,13 +186,14 @@ echo POLYBAR CONFIG IS MADE AND APPLIED
 
 # INSTALL THE THEMES
 if [ -d "$HOME/.themes" ]; then
-sudo cp -f ./Graphite-Dark $HOME/.themes
+sudo cp -rf ./Graphite-Dark $HOME/.themes
 chmod -R a+rw $HOME/.themes/
 else
 sudo mkdir $HOME/.themes
-sudo cp -f ./Graphite-Dark $HOME/.themes
+sudo cp -rf ./Graphite-Dark $HOME/.themes
 chmod -R a+rw $HOME/.themes/
 fi
+
 clear
 echo PACKAGE IS SYNCHRONIZED 
 echo UNIMATRIX DOWNLOADED
@@ -206,7 +214,6 @@ echo THEMES FOLDER IS CREATED AND GRAPHITE-DARK THEME IS MOVED FOR THERE
 # RELOAD THE POLYBAR AND THE I3 TO USED THE NEW CONFIG
 i3-msg restart
 polybar-msg cmd restart
-
 
 clear
 echo PACKAGE IS SYNCHRONIZED 
@@ -231,7 +238,6 @@ cd ./gnome-terminal-transparency
 makepkg
 sudo pacman -U gnome-terminal-transparency-3.46.8-1-x86_64.pkg.tar.zst
 cd $HOME/HaruRice
-
 
 clear
 echo PACKAGE IS SYNCHRONIZED 
