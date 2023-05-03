d778891d94fb8aa2469b314c9726ae5389fd5884
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-05-03 11:35:23 -0300

    Atualizar o Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -270,13 +270,14 @@ echo GRAPHITE-DARK THEME IS MOVED FOR .THEMES
 echo I3 AND POLYBAR SUCESSFULY RESTARTED
 
 
-
 # INSTALL THE GNOME-TERMINAL-TRANSPARENCY
 cd "$HOME/HaruRice/"
 cd "./gnome-terminal-transparency"
 makepkg
 sudo pacman -U gnome-terminal-transparency-3.46.8-1-x86_64.pkg.tar.zst
 cd "$HOME/HaruRice"
+
+sudo pacman -S gvfs
 
 clear
 echo PACKAGE IS SYNCHRONIZED.
