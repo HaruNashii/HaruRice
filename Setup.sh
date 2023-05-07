4884211de2f1861c3f794fb902ac5c221cbf580d
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-05-07 14:39:51 -0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -7,19 +7,19 @@ sudo chmod a+rwx ./Scripts/apps.sh
 
 # REMOVE THE BLOCK OF THE PACMAN IF EXIST
 # UPDATE THE SYSTEM AND INSTALL THE DEPENDENCIES
-exec ./Scripts/dependencies.sh
+./Scripts/dependencies.sh
 
 # MAKE A BACKUP OF YOUR (i3, polybar, rofi and picom.) CONFIGURATION, THE LOCALIZATION OF THE BACKUP IS ON README ON GITHUB
-exec ./Scripts/backups.sh
+./Scripts/backups.sh
 
 # EXPORT ALL THE HARURICE CONFIGS OF (i3, polybar, rofi and picom.) FOR YOUR SYSTEM
-exec ./Scripts/configs.sh
+./Scripts/configs.sh
 
 # EXPORT ALL FONTS THAT IS USED IN THE RICE FOR YOUR SYSTEM
-exec ./Scripts/fonts.sh
+./Scripts/fonts.sh
 
 # INSTALL ALL APPS THAT IS CONFIGURED ON "i3" YOU CAN REMOVE THIS LINE IF YOU DON'T WANT THE APPS BUY YOU WILL NEED TO EDIT THE i3 CONFIG FILE BY YOURSELF
-exec ./Scripts/apps.sh
+./Scripts/apps.sh
 
 # CLEAN THE TERMINAL
 clear
