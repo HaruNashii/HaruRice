9f0eaff8799f46494c536504cbcab19cb98ae16d
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-05-07 16:21:47 -0300

    Update i3configv1.sh

--- Scripts/i3configv1.sh
+++ Scripts/i3configv1.sh
@@ -1,13 +1,11 @@
 # MAKE A BACKUP OF YOUR OLD I3 CONFIG ON ($HOME/.config/i3/backup) AND ACTIVE THE RICE ONE ON ($HOME/.config/i3/)
 if [ -d "$HOME/.config/i3" ]; then
-cd "$HOME/HaruRice"
-sudo cp -f "./i3-config/config" "$HOME/.config/i3/config"
+sudo cp -rf "$HOME/HaruRice/i3-config/config" "$HOME/.config/i3/config"
 sudo chmod -R a+rw "$HOME/.config/i3/"
 sudo chmod a+rw "$HOME/.config/i3/*"
 else
-cd "$HOME/HaruRice"
 sudo mkdir "$HOME/.config/i3"
-sudo cp -f "./i3-config/config" "$HOME/.config/i3/config"
+sudo cp -rf "$HOME/HaruRice/i3-config/config" "$HOME/.config/i3/config"
 sudo chmod -R a+rw "$HOME/.config/i3/"
 sudo chmod a+rw "$HOME/.config/i3/*"
 fi
