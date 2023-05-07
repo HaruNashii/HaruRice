16a634ea7b91bf5fa41e8c236cde2abfb735edc9
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-05-07 15:15:09 -0300

    Update i3configv1.sh

--- Scripts/i3configv1.sh
+++ Scripts/i3configv1.sh
@@ -5,7 +5,7 @@ sudo cp -f "./i3-config/config" "$HOME/.config/i3/config"
 sudo chmod -R a+rw "$HOME/.config/i3/"
 sudo chmod a+rw "$HOME/.config/i3/*"
 else
-"$HOME/HaruRice"
+cd "$HOME/HaruRice"
 sudo mkdir "$HOME/.config/i3"
 sudo cp -f "./i3-config/config" "$HOME/.config/i3/config"
 sudo chmod -R a+rw "$HOME/.config/i3/"
