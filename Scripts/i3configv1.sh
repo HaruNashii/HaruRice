70eec9a029d7e421876929f769d777a51cbea1c6
Haru <116776134+HaruNashii@users.noreply.github.com>
2023-05-25 14:31:34 -0300

    Update i3configv1.sh

--- Scripts/i3configv1.sh
+++ Scripts/i3configv1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # MAKE A BACKUP OF YOUR OLD I3 CONFIG ON ($HOME/.config/i3/backup) AND ACTIVE THE RICE ONE ON ($HOME/.config/i3/)
 if [ -d "$HOME/.config/i3" ]; then
 sudo cp -rf "$HOME/HaruRice/i3-config/config" "$HOME/.config/i3/config"
